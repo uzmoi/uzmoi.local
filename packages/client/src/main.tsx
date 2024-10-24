092f822163e5ca96eb1e6a980edc4fec91e0f28c
uzmoi <rizzzse@gmail.com>
2024-10-25 03:38:17 +0900

    add TanStack Router

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { routeTree } from './routeTree.gen'
 
-createRoot(document.getElementById('root')!).render(
+// Set up a Router instance
+const router = createRouter({
+  routeTree,
+  defaultPreload: 'intent',
+})
+
+// Register things for typesafety
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
+const rootElement = document.getElementById('root')!
+
+const root = createRoot(rootElement)
+root.render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
