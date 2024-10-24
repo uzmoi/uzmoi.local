33353ba7022b1ee4c7d8be43c2b62a603188b966
uzmoi <rizzzse@gmail.com>
2024-10-25 02:51:05 +0900

    add /save-file endpoint

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -6,4 +6,16 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
+app.post('/save-file', async (c) => {
+  const { file_name } = c.req.query();
+  const blob = await c.req.blob();
+  const file = Bun.file(`./${file_name}`);
+  if (await file.exists()) {
+    return c.json({ ok: false });
+  }
+
+  await Bun.write(file, blob);
+  return c.json({ ok: true });
+});
+
 export default app
